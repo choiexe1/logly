4d4d52364fc1aad713a9005a4200974e90153937
choiexe1 <choiexe1@gmail.com>
2025-01-16 21:26:34 +0900

    비동기 자바 메일 센더 구현

---
+++ src/main/java/app/logly/helper/mail/MailHelper.java
@@ -0,0 +1,41 @@
+package app.logly.helper.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+@Component
+@RequiredArgsConstructor
+public class MailHelper {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async("mailExecutor")
+    public void send(String to, String subject, String body) {
+        MimeMessagePreparator messagePreparator =
+                mimeMessage -> {
+                    final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                    helper.setFrom("noreply@noreply.com");
+                    helper.setTo(to);
+                    helper.setSubject(subject);
+                    helper.setText(body, true);
+                };
+
+        mailSender.send(messagePreparator);
+    }
+
+    @Async("mailExecutor")
+    public void sendVerifyCode(String to, int verifyCode) {
+        Context context = new Context();
+        context.setVariable("verifyCode", verifyCode);
+        String subject = "로글리 회원가입 인증 코드";
+        String body = templateEngine.process("email/verifyCode.html", context);
+        send(to, subject, body);
+    }
+
+}
