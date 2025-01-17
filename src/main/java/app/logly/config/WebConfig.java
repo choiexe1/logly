8b98b2a35badb26df2205f96d82c762adf152be2
choiexe1 <choiexe1@gmail.com>
2025-01-17 11:59:47 +0900

    HTTP 특정 메서드만 허용 (allowedMethods)

--- src/main/java/app/logly/config/WebConfig.java
+++ src/main/java/app/logly/config/WebConfig.java
@@ -5,6 +5,7 @@ import app.logly.web.interceptor.AuthenticationInterceptor;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SIDArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST");
     }
 }
