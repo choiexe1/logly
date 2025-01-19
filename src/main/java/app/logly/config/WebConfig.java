d2718f5d74b52195a06578cb7206798d087af35c
choiexe1 <choiexe1@gmail.com>
2025-01-20 01:27:46 +0900

    웹 구성정보에 공통 모델 인터셉터 적용

--- src/main/java/app/logly/config/WebConfig.java
+++ src/main/java/app/logly/config/WebConfig.java
@@ -1,25 +1,30 @@
 package app.logly.config;
 
+import app.logly.service.MemberService;
 import app.logly.web.interceptor.AuthenticationInterceptor;
 import app.logly.web.interceptor.CommonModelInterceptor;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private static final List<String> STATICS = List.of("/css/**", "logly.ico", "/terms");
     private static final List<String> PUBLICS = List.of("/login", "/register");
+
+    private final MemberService memberService;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthenticationInterceptor())
                 .order(1)
                 .excludePathPatterns(mergeLists(STATICS, PUBLICS));
-        registry.addInterceptor(new CommonModelInterceptor())
+        registry.addInterceptor(new CommonModelInterceptor(memberService))
                 .order(2)
                 .excludePathPatterns(mergeLists(STATICS, PUBLICS));
     }
