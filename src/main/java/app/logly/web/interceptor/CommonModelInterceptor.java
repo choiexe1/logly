074df8688984ecac82a8a398590af9f9b5d5d3e3
choiexe1 <choiexe1@gmail.com>
2025-01-20 16:56:39 +0900

    공통 모델 인터셉터, GET 요청에만 작동하도록 변경

--- src/main/java/app/logly/web/interceptor/CommonModelInterceptor.java
+++ src/main/java/app/logly/web/interceptor/CommonModelInterceptor.java
@@ -5,6 +5,7 @@ import app.logly.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,17 +17,23 @@ public class CommonModelInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
                            ModelAndView modelAndView) throws Exception {
-        if (modelAndView != null) {
-            modelAndView.addObject("member", getMember(request));
-            modelAndView.addObject("currentUri", request.getRequestURI());
+        if (!request.getMethod().equals("POST") && modelAndView != null) {
+            Map<String, Object> model = modelAndView.getModel();
+
+            model.put("member", getMember(request));
+            model.put("currentUri", request.getRequestURI());
         }
     }
 
     private Member getMember(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-        Long id = (Long) session.getAttribute("id");
 
-        return memberService.findByIdOrThrow(id);
+        if (session != null) {
+            Long id = (Long) session.getAttribute("id");
+            return memberService.findByIdOrThrow(id);
+        }
+
+        return null;
     }
 
 }
