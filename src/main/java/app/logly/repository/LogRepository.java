b2a6043a355cd60f12cf29b35bfbfe0d2c3e3ec2
choiexe1 <choiexe1@gmail.com>
2025-01-20 19:56:45 +0900

    이름 변경

--- src/main/java/app/logly/repository/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package app.logly.repository;
-
-import app.logly.domain.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-}
