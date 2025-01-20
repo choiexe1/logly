6fd04ef8299e5319dc549c9117e9971e4fdaee83
choiexe1 <choiexe1@gmail.com>
2025-01-20 19:44:50 +0900

    모델 버그 수정

--- src/main/java/app/logly/web/advice/CommonModelAttributeAdvice.java
+++ src/main/java/app/logly/web/advice/CommonModelAttributeAdvice.java
@@ -2,15 +2,19 @@ package app.logly.web.advice;
 
 import app.logly.domain.Member;
 import app.logly.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @ControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class CommonModelAttributeAdvice {
     private final MemberService memberService;
 
@@ -21,6 +25,10 @@ public class CommonModelAttributeAdvice {
             model.addAttribute("member", member);
         }
 
-        model.addAttribute("currentUri", ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String currentUri = request.getRequestURI();
+        model.addAttribute("currentUri", currentUri);
+        log.info("currentUri = {}", currentUri);
+        log.info(" = {}", model.getAttribute("currentUri"));
     }
 }
